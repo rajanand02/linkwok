925f56170422cc8f254f1752f6febf5dbb7c8fa4
rajanand02 <rajanand0202@gmail.com>
2014-08-28 19:22:35 +0530

    add move features to front page

--- js/custom.js
+++ js/custom.js
@@ -3,15 +3,18 @@ $(document).ready(function(){
     e.preventDefault();
     $('.youtube').attr('src', '//www.youtube.com/embed/ONLDszjnQCw');
     $(".open-video").hide()
-    $(".close-video").show()
+    $(".close-video, .video-block").show()
     $(".content-block").addClass("open")
 
   })
   $(".close-video").click(function(e){
     e.preventDefault();
-    $(".close-video").hide()
+    $(".close-video, .video-block").hide()
     $(".open-video").show();
     $(".content-block").removeClass("open")
     $('.youtube').attr('src', '');
-  })
+  });
+  $(".getting-started").click(function(){
+    window.location="https://app.linkwok.com"
+  });
 })
